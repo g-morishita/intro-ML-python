349027d4e3a0f609f0f711a4515f2deb8ecbb0c6
gota_morishita <optimistic.gota@gmail.com>
2018-10-15 14:12:10 +0900

    moved the files to knn directory

--- ch2/plot_forge.py
+++
@@ -1,7 +0,0 @@
-import mglearn
-import matplotlib.pyplot as plt
-
-# generate dataset
-X, y = mglearn.datasets.make_forge()
-mglearn.discrete_scatter(X[:, 0], X[:, 1], y)
-plt.legend(["Class 0", "Class 1"], loc=4)
