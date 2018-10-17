b98dd305985a9973e8f84774a71877c9ebabcde7
gota_morishita <optimistic.gota@gmail.com>
2018-10-17 10:37:59 +0900

    implemented pruned tree

---
+++ ch2/tree/pruned_tree.py
@@ -0,0 +1,13 @@
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_breast_cancer
+
+cancer = load_breast_cancer()
+train_X, test_X, train_y, test_y = train_test_split(
+    cancer.data, cancer.target, stratify=cancer.target, random_state=42)
+
+tree = DecisionTreeClassifier(max_depth=4, random_state=0)
+tree.fit(train_X, train_y)
+
+print(f"Accuracy on training set: {tree.score(train_X, train_y)}")
+print(f"Accuracy on test set: {tree.score(test_X, test_y)}")
