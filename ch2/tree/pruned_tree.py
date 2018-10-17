162fdf1ee2a54ac7e5a1ba4110d61fc0d8c15ea8
gota_morishita <optimistic.gota@gmail.com>
2018-10-17 14:33:41 +0900

    modified tree alg

--- ch2/tree/pruned_tree.py
+++ ch2/tree/pruned_tree.py
@@ -1,8 +1,9 @@
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_breast_cancer
 
 cancer = load_breast_cancer()
+print(cancer.target_names)
 train_X, test_X, train_y, test_y = train_test_split(
     cancer.data, cancer.target, stratify=cancer.target, random_state=42)
 
@@ -11,3 +12,6 @@ tree.fit(train_X, train_y)
 
 print(f"Accuracy on training set: {tree.score(train_X, train_y)}")
 print(f"Accuracy on test set: {tree.score(test_X, test_y)}")
+
+export_graphviz(tree, out_file="tree.dot", class_names=cancer.target_names,
+                feature_names=cancer.feature_names, impurity=False, filled=True)
