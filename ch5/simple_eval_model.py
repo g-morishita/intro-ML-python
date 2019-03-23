82e1acbc119d91757f39e0b3008e8f60e0c8ae40
gota_morishita <optimistic.gota@gmail.com>
2019-03-23 17:55:47 +0900

    ch5: simple evaluate a model

---
+++ ch5/simple_eval_model.py
@@ -0,0 +1,13 @@
+from sklearn.datasets import make_blobs
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+
+# create a synthetic dataset
+X, y = make_blobs(random_state=0)
+
+# split data and labels into a training and a test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+# instantiate a model and fit it to the training set
+logreg = LogisticRegression().fit(X_train, y_train)
+# evaluate the model on the test set
+print(f"Test set score: {logreg.score(X_test, y_test):.2f}")
